60240d38613f7a1f389cdf50444f9eda23e9c0df
Imran latif <159417772+Imranlat@users.noreply.github.com>
2024-03-10 12:42:00 +0500

    Add files via upload

---
+++ q15.js
@@ -0,0 +1,14 @@
+"use strict";
+//15. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+let guestList = ["Ali", "Kamal", "Murad"];
+console.log(`Dear ${guestList[0]}, you are invited to Hi-tea.`);
+console.log(`Dear ${guestList[1]}, you are invited to Hi-tear.`);
+console.log(`Dear ${guestList[2]}, you are invited to dinner.`);
+// Someone can't make it
+let unableToAttend = guestList[1];
+console.log(`${unableToAttend} can't make it to the Hi-tea.`);
+// Replace with a new guest
+guestList[1] = "Kamran";
+console.log(`Dear ${guestList[0]}, you are invited to Hi-tea.`);
+console.log(`Dear ${guestList[1]}, you are invited to Hi-tea.`);
+console.log(`Dear ${guestList[2]}, you are invited to Hi-tea.`);
